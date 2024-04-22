31c9ea6386f8e165d4f2423fbcfd5d848a20867a
Putra P <marojahan.purba@mncgroup.com>
2024-04-22 23:14:14 +0700

    add versioning through URI Path

--- src/main/java/com/example/transfersystem/controller/BankController.java
+++ src/main/java/com/example/transfersystem/controller/BankController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/v1/")
 public class BankController {
 
     private BankService bankService;
--- src/main/java/com/example/transfersystem/controller/UserController.java
+++ src/main/java/com/example/transfersystem/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/v1/")
 public class UserController {
     private UserService userService;
 
